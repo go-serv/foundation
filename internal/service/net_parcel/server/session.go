5e2b1f332f38d144dd12b3dc828ec9edf4e169b8
Andrei Samuilik <a.samuylik@gmail.com>
2022-06-08 16:58:39 +0300

    minor

--- internal/service/net_parcel/server/session.go
+++ internal/service/net_parcel/server/session.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"crypto/rand"
+	"fmt"
 	proto "github.com/go-serv/service/internal/autogen/proto/net"
 	"github.com/go-serv/service/internal/grpc/meta/net"
 	"github.com/go-serv/service/internal/grpc/session"
@@ -11,20 +12,22 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+const NonceMaxLength = 32
+
 func (s serviceImpl) SecureSession(ctx context.Context, req *proto.Session_Request) (res *proto.Session_Response, err error) {
 	var (
 		nonce, encKey []byte
 	)
 	netCtx := ctx.(z.NetContextInterface)
 	res = &proto.Session_Response{}
-	// Create nonce
-	if req.GetNonceLength() > 32 {
-		return nil, status.Error(codes.InvalidArgument, "nonce length exceeds the maximum value of 32")
+	// Create a nonce
+	if req.GetNonceLength() > NonceMaxLength {
+		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("nonce length exceeds the maximum value of %d", NonceMaxLength))
 	}
-	nonce = make([]byte, req.GetNonceLength())
+	nonce = make([]byte, req.GetNonceLength(), NonceMaxLength)
 	rand.Read(nonce)
 	res.Nonce = nonce
-	// Create session
+	// Create a new session
 	lifetime := uint16(req.GetLifetime())
 	sess := session.NewSecureSession(lifetime, nonce, encKey)
 	dic := netCtx.Response().Meta().Dictionary().(*net.HttpClientDictionary)
